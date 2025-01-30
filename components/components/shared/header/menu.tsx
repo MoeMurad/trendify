71a3a1ed936a0a8bfbf41bd655481e27240f031c
MoeMurad <123353319+MoeMurad@users.noreply.github.com>
2025-01-30 12:31:13 +0800

    10-create-add-to-cart-component

--- components/components/shared/header/menu.tsx
+++ components/components/shared/header/menu.tsx
@@ -1,27 +1,18 @@
-import { ShoppingCartIcon } from 'lucide-react'
 import Link from 'next/link'
+import CartButton from './cart-button'
 
 export default function Menu() {
-	return (
-		<div className='flex justify-end'>
-			<nav className='flex gap-3 w-full'>
-				<Link
-					href='/signin'
-					className='flex items-center header-button'
-				>
-					Sign In
-				</Link>
-
-				<Link
-					href='/cart'
-					className='header-button'
-				>
-					<div className='flex items-end gap-1'>
-						<ShoppingCartIcon className='h-8 w-8' />
-						Cart
-					</div>
-				</Link>
-			</nav>
-		</div>
-	)
+    return (
+        <div className="flex justify-end">
+            <nav className="flex gap-3 w-full">
+                <Link
+                    href="/signin"
+                    className="flex items-center header-button"
+                >
+                    Sign In
+                </Link>
+                <CartButton />
+            </nav>
+        </div>
+    )
 }
--- components/shared/product/select-variant.tsx
+++ components/shared/product/select-variant.tsx
@@ -4,82 +4,82 @@ import Link from 'next/link'
 import { cn } from '@/lib/utils'
 
 export default function SelectVariant({
-	product,
-	size,
-	color,
+    product,
+    size,
+    color,
 }: {
-	product: IProduct
-	color: string
-	size: string
+    product: IProduct
+    color: string
+    size: string
 }) {
-	const selectedColor = color || product.colors[0]
-	const selectedSize = size || product.sizes[0]
+    const selectedColor = color || product.colors[0]
+    const selectedSize = size || product.sizes[0]
 
-	return (
-		<>
-			{product.colors.length > 0 && (
-				<div className='space-x-2 space-y-2'>
-					<div>Color:</div>
-					{product.colors.map((x: string) => (
-						<Button
-							asChild
-							variant='outline'
-							className={cn(
-								'border',
-								selectedColor === x
-									? 'border-primary ring-2 ring-primary/50 bg-primary/5'
-									: 'border-input'
-							)}
-							key={x}
-						>
-							<Link
-								replace
-								scroll={false}
-								href={`?${new URLSearchParams({
-									color: x,
-									size: selectedSize,
-								})}`}
-								key={x}
-							>
-								<div
-									style={{ backgroundColor: x }}
-									className='h-4 w-4 rounded-full border border-muted-foreground'
-								></div>
-								{x}
-							</Link>
-						</Button>
-					))}
-				</div>
-			)}
-			{product.sizes.length > 0 && (
-				<div className='mt-2 space-x-2 space-y-2'>
-					<div>Size:</div>
-					{product.sizes.map((x: string) => (
-						<Button
-							asChild
-							variant='outline'
-							className={cn(
-								'border',
-								selectedSize === x
-									? 'border-primary ring-2 ring-primary/50 bg-primary/5'
-									: 'border-input'
-							)}
-							key={x}
-						>
-							<Link
-								replace
-								scroll={false}
-								href={`?${new URLSearchParams({
-									color: selectedColor,
-									size: x,
-								})}`}
-							>
-								{x}
-							</Link>
-						</Button>
-					))}
-				</div>
-			)}
-		</>
-	)
+    return (
+        <>
+            {product.colors.length > 0 && (
+                <div className="space-x-2 space-y-2">
+                    <div>Color:</div>
+                    {product.colors.map((x: string) => (
+                        <Button
+                            asChild
+                            variant="outline"
+                            className={cn(
+                                'border',
+                                selectedColor === x
+                                    ? 'border-primary ring-2 ring-primary/50 bg-primary/5'
+                                    : 'border-input'
+                            )}
+                            key={x}
+                        >
+                            <Link
+                                replace
+                                scroll={false}
+                                href={`?${new URLSearchParams({
+                                    color: x,
+                                    size: selectedSize,
+                                })}`}
+                                key={x}
+                            >
+                                <div
+                                    style={{ backgroundColor: x }}
+                                    className="h-4 w-4 rounded-full border border-muted-foreground"
+                                ></div>
+                                {x}
+                            </Link>
+                        </Button>
+                    ))}
+                </div>
+            )}
+            {product.sizes.length > 0 && (
+                <div className="mt-2 space-x-2 space-y-2">
+                    <div>Size:</div>
+                    {product.sizes.map((x: string) => (
+                        <Button
+                            asChild
+                            variant="outline"
+                            className={cn(
+                                'border',
+                                selectedSize === x
+                                    ? 'border-primary ring-2 ring-primary/50 bg-primary/5'
+                                    : 'border-input'
+                            )}
+                            key={x}
+                        >
+                            <Link
+                                replace
+                                scroll={false}
+                                href={`?${new URLSearchParams({
+                                    color: selectedColor,
+                                    size: x,
+                                })}`}
+                            >
+                                {x}
+                            </Link>
+                        </Button>
+                    ))}
+                </div>
+            )}
+        </>
+    )
 }
