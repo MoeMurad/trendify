e6658e99713b8907b635ff0f2560035ac7307694
MoeMurad <123353319+MoeMurad@users.noreply.github.com>
2025-01-31 17:33:46 +0800

    13-create-sign-in-page-&-implementing-users

--- types/index.ts
+++ types/index.ts
@@ -1,25 +1,32 @@
 import {
-	CartSchema,
-	OrderItemSchema,
-	ProductInputSchema,
+    CartSchema,
+    OrderItemSchema,
+    ProductInputSchema,
+    UserInputSchema,
+    UserSignInSchema,
 } from '@/lib/validator'
 import { z } from 'zod'
 
 export type IProductInput = z.infer<typeof ProductInputSchema>
 
 export type Data = {
-	products: IProductInput[]
-	headerMenus: {
-		name: string
-		href: string
-	}[]
-	carousels: {
-		image: string
-		url: string
-		title: string
-		buttonCaption: string
-		isPublished: boolean
-	}[]
+    users: IUserInput[]
+    products: IProductInput[]
+    headerMenus: {
+        name: string
+        href: string
+    }[]
+    carousels: {
+        image: string
+        url: string
+        title: string
+        buttonCaption: string
+        isPublished: boolean
+    }[]
 }
 export type OrderItem = z.infer<typeof OrderItemSchema>
 export type Cart = z.infer<typeof CartSchema>
+
+// user
+export type IUserInput = z.infer<typeof UserInputSchema>
+export type IUserSignIn = z.infer<typeof UserSignInSchema>
